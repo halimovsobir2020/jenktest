ca076449411a8ae0f732153f87ad55237315c3cf
Your NameAbdusobur <a@gmail.com>
2025-07-21 05:01:25 +0500

    first commit

---
+++ src/main/java/uz/pdp/jenktest/JenktestApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.jenktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/jenktest/JenktestApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.jenktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
